51a9d7868568fd99ab752deced946f5d8c6b1ab9
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-05-10 10:59:14 +0200

    Add rinkeby address to config

--- packages/web/config.ts
+++ packages/web/config.ts
@@ -2,8 +2,14 @@ import ProjectFactory from "contracts/ProjectFactory.json";
 import Project from "contracts/Project.json";
 import ERC20Token from "contracts/ERC20Token.json";
 
-export const PROJECT_FACTORY_ADDRESS =
-  "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const contractAddresses = {
+  31337: {
+    projectFactory: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+  },
+  4: {
+    projectFactory: "0x9bC9E782C7A21FDFacFFCF88a6200eB47d555856",
+  },
+};
 
 export const contractTypes = {
   project: Project.abi,
