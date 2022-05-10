21edc23215797825ad97fbfa85dfef787bcc5c86
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-05-10 13:37:26 +0200

    Hardcode projects on LandingPage

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -3,44 +3,35 @@ import type { NextPage } from "next";
 import Layout from "components/Layout";
 import { Box, Heading, Image, SimpleGrid } from "@chakra-ui/react";
 import Link from "next/link";
-import { useGithubContent } from "modules/project/hooks/useGithubContent";
 
-const projects = ["carlbarrdahl/git-dao"];
+const projects = [
+  {
+    repo: "carlbarrdahl/git-dao",
+    img: "https://raw.githubusercontent.com/carlbarrdahl/git-dao/master/.gitdao/screenshot.png",
+  },
+  {
+    repo: "carlbarrdahl/kaly",
+    img: "https://kaly.vercel.app/kaly.jpg",
+  },
+];
 const Landing: NextPage = ({}) => {
   return (
     <Layout>
       <Heading mb={8}>Browse projects</Heading>
-      <SimpleGrid columns={[1, 1, 2, 3, 4]}>
-        {projects.map((repo) => (
-          <ProjectCard repo={repo} key={repo} />
+      <SimpleGrid columns={[1, 1, 2, 3, 4]} spacing={16}>
+        {projects.map((project) => (
+          <ProjectCard key={project.repo} {...project} />
         ))}
       </SimpleGrid>
     </Layout>
   );
 };
 
-function ProjectCard({ repo }) {
-  const { data } = useGithubContent({
-    repo,
-    content: ".gitdao/screenshot.png",
-    responseType: "arraybuffer",
-  });
-
-  let imgSrc;
-
-  try {
-    console.log(data);
-    imgSrc = `data:${"image/png"};base64,${Buffer.from(
-      data || "",
-      "binary"
-    ).toString("base64")}`;
-  } catch (error) {
-    console.log(error);
-  }
+function ProjectCard({ repo, img }) {
   return (
     <Link href={`/${repo}`} passHref>
-      <Box as="a" boxShadow={"2xl"} borderWidth={1} borderColor={"gray.300"}>
-        <Image alt={repo} fit={"cover"} src={imgSrc} minH={300} />
+      <Box as="a" boxShadow={"xl"} borderWidth={1} borderColor={"gray.300"}>
+        <Image alt={repo} fit={"cover"} src={img} />
         <Box p={4}>
           <Heading fontSize="md">{repo}</Heading>
         </Box>
