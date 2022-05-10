3798260edce29656419b70d5a74c65967cc8e5d2
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-05-10 09:27:41 +0200

    Fix merge

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
-}
-
-module.exports = nextConfig
-=======
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
 
 module.exports = nextConfig;
->>>>>>> aed353b94a6bad2bca1e3cca96f6b08a01cf424f
