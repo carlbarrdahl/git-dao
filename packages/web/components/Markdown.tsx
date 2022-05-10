dc012b2842ece11e1151cb80a78d41b4697217ff
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-05-10 09:28:21 +0200

    Improve markdown rendering

--- packages/web/components/Markdown.tsx
+++ packages/web/components/Markdown.tsx
@@ -8,21 +8,24 @@ import {
   UnorderedList,
   ListItem,
   Image,
+  Link,
 } from "@chakra-ui/react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 const components = {
-  h1: (p) => <Heading as="h1" fontSize="4xl" {...p} />,
-  h2: (p) => <Heading as="h2" fontSize="2xl" {...p} />,
-  h3: (p) => <Heading as="h3" fontSize="xl" {...p} />,
-  h4: (p) => <Heading as="h3" fontSize="lg" {...p} />,
-  p: (p) => <Text mb={8} fontSize="lg" {...p} />,
-  ul: (p) => <UnorderedList {...p} ml={8} mb={16} />,
-  ol: (p) => <OrderedList {...p} ml={8} mb={16} />,
-  li: (p) => <ListItem {...p} fontSize="xl" mb={4} />,
+  h1: (p) => <Heading as="h1" fontSize="4xl" {...p} mb={6} />,
+  h2: (p) => <Heading as="h2" fontSize="3xl" {...p} mb={4} />,
+  h3: (p) => <Heading as="h3" fontSize="xl" {...p} mb={4} />,
+  h4: (p) => <Heading as="h3" fontSize="lg" mb={4} {...p} />,
+  h5: (p) => <Heading as="h3" fontSize="md" mb={4} {...p} />,
+  h6: (p) => <Heading as="h3" fontSize="sm" mb={4} {...p} />,
+  p: (p) => <Text fontSize="lg" {...p} mb={4} />,
+  ul: (p) => <UnorderedList {...p} ml={8} mb={4} />,
+  ol: (p) => <OrderedList {...p} ml={8} mb={4} />,
+  li: (p) => <ListItem {...p} fontSize="lg" mb={2} />,
   img: (p) => <Image {...p} my={8} />,
-  a: (p) => <a {...p} my={8} />,
+  a: (p) => <Link color="blue.500" {...p} my={8} />,
   hr: (p) => <Divider {...p} my={8} />,
 };
 
