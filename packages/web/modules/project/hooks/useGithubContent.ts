a935432fa72425a4e8f789aa9b4f9c7974ac7b93
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-05-10 13:25:15 +0200

    Browse projects on Landing Page

--- packages/web/modules/project/hooks/useGithubContent.ts
+++ packages/web/modules/project/hooks/useGithubContent.ts
@@ -1,17 +1,18 @@
 import axios from "lib/axios";
 import { useQuery } from "react-query";
-import { useSession } from "next-auth/react";
 
-interface Repo {
-  name: string;
-  full_name: string;
-  private: boolean;
-  pushed_at: string;
-}
-export function useGithubContent(params: { repo: string; content: string }) {
-  const path = `https://raw.githubusercontent.com/${params.repo}/master/${params.content}`;
+export function useGithubContent({
+  repo,
+  content,
+  ...params
+}: {
+  repo: string;
+  content: string;
+  responseType?: string;
+}) {
+  const path = `https://raw.githubusercontent.com/${repo}/master/${content}`;
 
-  return useQuery([path], async () => axios.get(path), {
-    enabled: Boolean(params.repo),
+  return useQuery([path], async () => axios.get(path, params), {
+    enabled: Boolean(repo),
   });
 }
--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,16 +1,52 @@
 import type { NextPage } from "next";
 
-import { useRouter } from "next/router";
 import Layout from "components/Layout";
-import { useEffect } from "react";
+import { Box, Heading, Image, SimpleGrid } from "@chakra-ui/react";
+import Link from "next/link";
+import { useGithubContent } from "modules/project/hooks/useGithubContent";
 
+const projects = ["carlbarrdahl/git-dao"];
 const Landing: NextPage = ({}) => {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/onboard");
-  }, [router]);
-
-  return <Layout>LandingPage</Layout>;
+  return (
+    <Layout>
+      <Heading mb={8}>Browse projects</Heading>
+      <SimpleGrid columns={[1, 1, 2, 3, 4]}>
+        {projects.map((repo) => (
+          <ProjectCard repo={repo} key={repo} />
+        ))}
+      </SimpleGrid>
+    </Layout>
+  );
 };
+
+function ProjectCard({ repo }) {
+  const { data } = useGithubContent({
+    repo,
+    content: ".gitdao/screenshot.png",
+    responseType: "arraybuffer",
+  });
+
+  let imgSrc;
+
+  try {
+    console.log(data);
+    imgSrc = `data:${"image/png"};base64,${Buffer.from(
+      data || "",
+      "binary"
+    ).toString("base64")}`;
+  } catch (error) {
+    console.log(error);
+  }
+  return (
+    <Link href={`/${repo}`} passHref>
+      <Box as="a" boxShadow={"2xl"} borderWidth={1} borderColor={"gray.300"}>
+        <Image alt={repo} fit={"cover"} src={imgSrc} minH={300} />
+        <Box p={4}>
+          <Heading fontSize="md">{repo}</Heading>
+        </Box>
+      </Box>
+    </Link>
+  );
+}
 
 export default Landing;
